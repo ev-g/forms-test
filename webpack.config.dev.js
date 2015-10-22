6a9c986836b075714244d0c23dc35806ae68dcf4
isomoar <isomoar@gmail.com>
2015-10-22 15:58:35 +0300

    Css loader

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -4,8 +4,7 @@ var webpack = require('webpack');
 module.exports = {
   devtool: 'eval',
   entry: [
-    'webpack-hot-middleware/client',
-    './src/index'
+    'webpack-hot-middleware/client', './src/index'
   ],
   output: {
     path: path.join(__dirname, 'dist'),
@@ -13,20 +12,25 @@ module.exports = {
     publicPath: '/static/'
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
+    new webpack.HotModuleReplacementPlugin(), new webpack.NoErrorsPlugin()
   ],
   module: {
     preLoaders: [
-     {
-       test: /\.js$/,
-       loader: "source-map-loader"
-     }
-   ],
-    loaders: [{
-      test: /\.js$/,
-      loaders: ['babel'],
-      include: path.join(__dirname, 'src')
-    }]
+      {
+        test: /\.js$/,
+        loader: "source-map-loader"
+      }
+    ],
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel'],
+        include: path.join(__dirname, 'src')
+      },
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css']
+      }
+    ]
   }
 };
