0143a1a7f547fc7e28c154ab375d745f20521efc
Jane <eg@4xxi.com>
2015-10-25 17:20:48 +0300

    Test commit

--- src/App.js
+++ src/App.js
@@ -5,11 +5,14 @@ import Form from './Form';
 class App extends React.Component {
   handleSubmit() {
     console.log('Sumitted!');
+    console.log("Test!");
+    var a = 1;
   }
 
   render () {
     return <div>
       <h1>Form example</h1>
+      <h3>Form here</h3>
       <div className="col-md-4 col-sm-3 col-lg-3 col-xs-6">
         <Form onSubmit={this.handleSubmit.bind(this)} />
       </div>
